49720563cfa5b29f2688f57ebadc8e1fff90ca86
Alex Seidler <alex.seidler@futurice.com>
2015-08-14 09:54:39 +0300

    Dummy react components for people, projects and powerhead

---
+++ src/app/people.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class People extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>People</h1>
+                <p>Lorem ipsum Voluptate ad nostrud irure ullamco pariatur Duis ut eiusmod Duis ex magna cupidatat sed ea labore.</p>                
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import polyfill from 'babel/polyfill';
+import React from 'react';
+import Router, {Route, Link, RouteHandler} from 'react-router';
+
+// Views
+import People from './app/people.js';
+import Projects from './app/projects.js';
+import Powerhead from './app/powerhead.js';
 
 console.log("Hello");
+
+class App extends React.Component {
+    render() {
+        return <RouteHandler />;
+    }
+}
+
+var routes = (
+    <Route name="app" path="/" handler={App}>
+        <Route name="people" handler={People} />
+        <Route name="projects" handler={Projects} />
+        <Route name="powerhead" handler={Powerhead} />
+    </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.querySelector('.content-wrapper'));
+});
