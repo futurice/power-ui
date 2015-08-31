5b5765b8a1e631daa7f2a174d0a006d0bf3bf218
Andre Medeiros <andre.medeiros@futurice.com>
2015-08-31 14:12:52 +0300

    Load people pages and incrementally update the peoples array

--- src/components/PeoplePage.js
+++ src/components/PeoplePage.js
@@ -121,11 +121,19 @@ function view(state$, locationFilterVTree$ = null, textFilterVTree$ = null,
 
 // Handle all HTTP networking logic of this page
 function PeoplePageHTTP(sources, urlRoot) {
-  const request$ = Rx.Observable.just(`${urlRoot}/people/`);
-  const response$ = sources.HTTP
-    .filter(res$ => res$.request === `${urlRoot}/people/`)
+  const PEOPLE_URL = `${urlRoot}/people/`;
+  const atomicResponse$ = sources.HTTP
+    .filter(res$ => _.startsWith(res$.request, PEOPLE_URL))
     .mergeAll()
-    .map(responseObj => responseObj.body.results);
+    .shareReplay(1);
+  const request$ = atomicResponse$
+    .filter(responseObj => responseObj.body.next)
+    .map(responseObj => responseObj.body.next.replace('limit=5', 'limit=40'))
+    .startWith(PEOPLE_URL + '?limit=5');
+  const arrayOfPeople$ = atomicResponse$
+    .map(responseObj => responseObj.body.results)
+    .scan((acc, curr) => acc.concat(curr));
+  const response$ = arrayOfPeople$;
   const sink = {
     request$,
     response$,
